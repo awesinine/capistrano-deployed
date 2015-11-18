4329ebdba4e3ceb56136549a9f99ebc70654dff0
Ryan Morten <awesinine@users.noreply.github.com>
2015-11-17 18:42:47 -0800

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+## this line below gets a sed edit that fills in the hostname for the config
 thing
+
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
